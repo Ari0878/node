1ffbd79744f079df3d92702df83c6f97fa38474d
Ari0878 <brisscadena29@gmail.com>
2026-05-20 19:59:04 -0600

    Servidor

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo");
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+app.listen(3000, (port) => {
+    console.log('escucha desde el puerto 3000');
+});
