6fdf3d979f6456ab7513e932f06ae90b3d25e947
Ari0878 <brisscadena29@gmail.com>
2026-05-30 09:15:30 -0600

    swagger

--- index.js
+++ index.js
@@ -21,19 +21,24 @@
 // });
 import express from "express";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.js";
+import swaggerSpec from "./config/swagger.js";
+
 
 dotenv.config();
-
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
 connectDB();
+app.use("/api/docs", 
+  swaggerUi.serve,  
+  swaggerUi.setup(swaggerSpec)
+);
 
 app.use("/api/users", userRoutes);
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,28 @@ import express from "express";
 const route = express.Router();
 import { createUser } from "../controllers/userController.js";
 
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Crea un nuevo usuario
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Brisset Hernandez
+ *     responses:
+ *       201:
+ *         description: Usuario creado
+ */
+
 route.post('/', createUser);
 
 export default route;
